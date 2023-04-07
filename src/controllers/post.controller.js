eee1a3f8fd07079cd3bb32f7fedf3edea17dcc60
indra-18 <indraind0918@gmail.com>
2023-04-07 18:11:09 +0530

    added result key to response

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,34 +2,39 @@ const db = require("../models");
 const Posts = db.posts
 
 var exports = module.exports = {}
-exports.create = (req, res) => {
+exports.create = async(req, res) => {
     if (!req.body) {
         res.status(400).send({message: 'request body not found'});
     }
-    const post = new Posts({
-        image: req.body.image,
-        author: req.body.author,
-        location: req.body.location,
-        description: req.body.description,
-        likes: req.body.likes,
-        date: req.body.date,
-        id: req.body.id
-    });
-    post.save().then(data => {
-        res.status(200).send(data);
-    }).catch(err => {
-        res.status(500).send({message: err.message})
-    })
-}
+    try {
+        let post = new Posts({
+            image: `image/${req.body.image}`,
+            author: req.body.author,
+            location: req.body.location,
+            description: req.body.description,
+            likes: req.body.likes,
+            date: req.body.date,
+            id: req.body.id
+        });
+        post = await post.save()
+        res.status(200).json({status: "Success", message: "Post Created", result: post});
+    }
+    catch(err) {
+            res.status(400).send({message: err.message})
+        }
+    }
 
-exports.all = (req, res) => {
-    Posts.find().then(data => {
-        if (data.length == 0) {
+exports.all = async(req, res) => {
+    try{
+        let posts = await Posts.find()
+        if (posts.length == 0) {
             return res.send({message: 'No Posts are there'})
         }
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).send({message: 'error while getting posts'})
-    })
+        res.status(200).json({status: "Success", result: posts});
+    }
+
+    catch(err) {
+        res.status(500).send({message: `error while getting posts => ${err.message}`})
+    }
 
 }
